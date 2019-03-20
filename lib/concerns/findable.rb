4391778a44319150036eff93426ebc661d9393a5
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-20 18:14:28 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,7 +4,7 @@ module Findable
   module ClassMethods
     def find_by_name(name)
       self.class_variables.first.detect {|a| a.name == name}
-      binding.pry
+      # binding.pry
     end
   end
 
